2fc814992a8d055df39a878e7565c663d45e138d
Monte Thakkar <mthakkar@mail.sfsu.edu>
2016-04-09 11:02:47 -0700

    added affine registration with mha data samples

---
+++ Registration-Affine-mha/CMakeFiles/ImageRegistrationAffine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImageRegistrationAffine.dir/ImageRegistrationAffine.cxx.o"
+  "ImageRegistrationAffine.pdb"
+  "ImageRegistrationAffine.app/Contents/MacOS/ImageRegistrationAffine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ImageRegistrationAffine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
