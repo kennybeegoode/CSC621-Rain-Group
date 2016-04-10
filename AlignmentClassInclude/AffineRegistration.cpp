38d02a731f62a5be5a9848685e7ef9bd91c720e2
Monte Thakkar <mthakkar@mail.sfsu.edu>
2016-04-10 13:35:20 -0700

    added alignment class files

---
+++ AlignmentClassInclude/AffineRegistration.cpp
@@ -0,0 +1,11 @@
+#include "AffineRegistration.hh" 
+
+using namespace std;
+
+
+void AffineRegistration::align(String fixedImage, string movingImage) {
+
+    //put everything that beliongs to main in here
+}
+
+
---
+++ AlignmentClassInclude/AffineRegistration.hh
@@ -0,0 +1,12 @@
+// in myclass.h
+
+#import all our include files here
+
+//Spine Curvature Calculator
+class AffineRegistration
+{
+public:
+	static void align(String fixedImage, string movingImage);
+};
+
+#endif
