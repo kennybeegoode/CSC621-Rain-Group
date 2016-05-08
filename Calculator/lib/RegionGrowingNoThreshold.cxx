9af3a9d8ee4b38022a494b99f6a4432d528db181
MARIA KHVALCHIK <maria.khvalchik@gmail.com>
2016-05-07 17:17:34 -0700

    add changes to region growing
    To ease the hassle of the seed point selection. We now iterate over the neighbors of the seed and find the point which produces the best segmentation result

--- Calculator/lib/RegionGrowingNoThreshold.cxx
+++ Calculator/lib/RegionGrowingNoThreshold.cxx
@@ -31,6 +31,9 @@
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 
+#define MAX(a, b) ((a) >= (b) ? (a) : (b))
+#define MIN(a, b) ((a) <= (b) ? (a) : (b))
+
 // Compute statistics variables.
 void RegionGrowingNoThreshold::ComputeStats(InternalImageType::Pointer& image,
                   std::unordered_map<long long, InternalImageType::IndexType>& region_points,
@@ -88,25 +91,26 @@ void RegionGrowingNoThreshold::ComputeThreshold(
   *upper_threshold = mgv + (upper_dev * 1.5 + 20 / sqrt(region_points.size()));
   *lower_threshold = mgv - (lower_dev * 1.5 + 20 / sqrt(region_points.size()));
 
-  std::cout << "mgv " << mgv << " upper_dev " << upper_dev << " lower_dev " << lower_dev << std::endl;
-  std::cout << "thresholds " << *upper_threshold << " " << *lower_threshold << std::endl;
+  //std::cout << "mgv " << mgv << " upper_dev " << upper_dev << " lower_dev " << lower_dev << std::endl;
+  //std::cout << "thresholds " << *upper_threshold << " " << *lower_threshold << std::endl;
 }
 
 // Compute thresholds for the final run.
 void RegionGrowingNoThreshold::ComputeFinalThreshold(
                            InternalImageType::Pointer& image,
                            std::unordered_map<long long, InternalImageType::IndexType>& region_points,
-                           double* upper_threshold, double* lower_threshold) {
-  double upper_dev, lower_dev, mgv;
-  ComputeStats(image, region_points, &mgv, &upper_dev, &lower_dev);
+                           double* upper_threshold, double* lower_threshold, double* upper_dev,
+                           double* lower_dev) {
+  double mgv;
+  ComputeStats(image, region_points, &mgv, upper_dev, lower_dev);
 
-  if (lower_dev == 0) lower_dev = 1;
-  if (upper_dev == 0) upper_dev = 1;
+  if (*lower_dev == 0) *lower_dev = 1;
+  if (*upper_dev == 0) *upper_dev = 1;
 
-  *upper_threshold = mgv + (upper_dev * 3.5 * 2.58 + 20 / sqrt(region_points.size()));
-  *lower_threshold = mgv - (lower_dev * 3.5 * 2.58 + 20 / sqrt(region_points.size()));
+  *upper_threshold = mgv + (*upper_dev * 2.3 * 2.58 + 20 / sqrt(region_points.size()));
+  *lower_threshold = mgv - (*lower_dev * 2.3 * 2.58 + 20 / sqrt(region_points.size()));
 
-  std::cout << "mgv " << mgv << " upper_dev " << upper_dev << " lower_dev " << lower_dev << std::endl;
+  std::cout << "mgv " << mgv << " upper_dev " << *upper_dev << " lower_dev " << *lower_dev << std::endl;
   std::cout << "final thresholds " << *upper_threshold << " " << *lower_threshold << std::endl;
 }
 
@@ -125,7 +129,7 @@ bool RegionGrowingNoThreshold::CheckBounds(
   return true;
 }
 
-void RegionGrowingNoThreshold::GrowRegions(
+bool RegionGrowingNoThreshold::GrowRegions(
                  InternalImageType::Pointer& image,
                  InternalImageType::IndexType& seed,
                  double* upper_threshold,
@@ -149,7 +153,7 @@ void RegionGrowingNoThreshold::GrowRegions(
           seed_copy[2] += k;
           if (!CheckBounds(seed_copy, size)) continue;
           q.push(seed_copy);
-          std::cout << "new point " << image->GetPixel(seed_copy) << std::endl;
+          //std::cout << "new point " << image->GetPixel(seed_copy) << std::endl;
           visited_points[ComputePointHash(seed_copy)] = seed_copy;
         }
       }
@@ -205,17 +209,29 @@ void RegionGrowingNoThreshold::GrowRegions(
       }
     }
   }
-
+  double upper_dev, lower_dev;
   // Finally, we need to compute the final thresholds for the second run.
-  ComputeFinalThreshold(image, visited_points, upper_threshold, lower_threshold);
+  ComputeFinalThreshold(image, visited_points, upper_threshold, lower_threshold,
+                        &upper_dev, &lower_dev);
+  // If deviation was too big, it returns false.
+  if (lower_dev >= 9 || upper_dev >= 9) {
+    return false;
+  }
+  return true;
 }
 
 std::vector<std::vector<int>> RegionGrowingNoThreshold::ComputeCentroids(
-  InternalImageType::Pointer& image) {
+  InternalImageType::Pointer& image, double* max_image_x_dist_ratio,
+  double* max_image_y_dist_ratio, double* max_image_z_dist_ratio) {
   std::vector<std::vector<int>> centroids;
   InternalImageType::RegionType region = image->GetLargestPossibleRegion();
   InternalImageType::SizeType size = region.GetSize();
+  *max_image_x_dist_ratio = -1;
+  *max_image_y_dist_ratio = -1;
+  *max_image_z_dist_ratio = -1;
+  long long z_max = -1, z_min = 100000;
   for (int z = 0; z < size[2]; z++) {
+      long long x_max = -1, x_min = 100000, y_max = -1, y_min = 100000;
       long long x_sum = 0, y_sum = 0;
       long long num = 0;
       for (int x = 0; x < size[0]; x++) {
@@ -225,6 +241,15 @@ std::vector<std::vector<int>> RegionGrowingNoThreshold::ComputeCentroids(
           pt[1] = y;
           pt[2] = z;
           if (image->GetPixel(pt) > 0) {
+            x_max = MAX(x, x_max);
+            x_min = MIN(x, x_min);
+            y_max = MAX(y, y_max);
+            y_min = MIN(y, y_min);
+            z_max = MAX(z, z_max);
+            z_min = MIN(z, z_min);
+            *max_image_x_dist_ratio = MAX(x_max - x_min, *max_image_x_dist_ratio);
+            *max_image_y_dist_ratio = MAX(y_max - y_min, *max_image_y_dist_ratio);
+            *max_image_z_dist_ratio = MAX(z_max - z_min, *max_image_z_dist_ratio);
             x_sum += x;
             y_sum += y;
             num++;
@@ -239,16 +264,23 @@ std::vector<std::vector<int>> RegionGrowingNoThreshold::ComputeCentroids(
         centroids.push_back(coord);
       }
   }
+  *max_image_x_dist_ratio = *max_image_x_dist_ratio / size[0];
+  *max_image_y_dist_ratio = *max_image_y_dist_ratio / size[1];
+  *max_image_z_dist_ratio = *max_image_z_dist_ratio / size[2];
+  std::cout << "max_x_dist_ratio " << *max_image_x_dist_ratio
+            << " max_y_dist_ratio " << *max_image_y_dist_ratio
+            << " max_z_dist_ratio " << *max_image_z_dist_ratio << std::endl;
+
   return centroids;
 }
 
 std::vector<std::vector<int>> RegionGrowingNoThreshold::GetCentroids(char filename[], int seed_x, int seed_y, int seed_z) {
   typedef  itk::ImageFileReader< InternalImageType > ReaderType;
-  typedef itk::CastImageFilter< InternalImageType, OutputImageType >
-                                                   CastingFilterType;
+  //typedef itk::CastImageFilter< InternalImageType, OutputImageType >
+  //                                                 CastingFilterType;
 
   ReaderType::Pointer reader = ReaderType::New();
-  CastingFilterType::Pointer caster = CastingFilterType::New();
+  //CastingFilterType::Pointer caster = CastingFilterType::New();
 
   reader->SetFileName( filename );
 
@@ -257,7 +289,7 @@ std::vector<std::vector<int>> RegionGrowingNoThreshold::GetCentroids(char filena
      CurvatureAnisotropicDiffusionImageFilterType;
   CurvatureAnisotropicDiffusionImageFilterType::Pointer smoothing =
      CurvatureAnisotropicDiffusionImageFilterType::New();
-  smoothing->SetNumberOfIterations(10);
+  smoothing->SetNumberOfIterations(10 );
   smoothing->SetTimeStep(0.020);
   smoothing->SetConductanceParameter(3);
 
@@ -273,38 +305,107 @@ std::vector<std::vector<int>> RegionGrowingNoThreshold::GetCentroids(char filena
   laplacian->SetInput(smoothing->GetOutput());
   diff->SetInput1(smoothing->GetOutput());
   diff->SetInput2(laplacian->GetOutput());
+  reader->Update();
+  InternalImageType::Pointer image = reader->GetOutput();
 
-  diff->Update();
-  InternalImageType::Pointer image = diff->GetOutput();
+  //typedef  itk::ImageFileWriter<  OutputImageType  > WriterType;
+  //WriterType::Pointer writer = WriterType::New();
+  //writer->SetFileName("output1.mhd");
 
   typedef itk::ConnectedThresholdImageFilter< InternalImageType,
        InternalImageType > ConnectedFilterType;
   ConnectedFilterType::Pointer connectedThreshold = ConnectedFilterType::New();
   // The color we are going to paint the region with.
   connectedThreshold->SetReplaceValue(255);
+
   // Setting up seeds.
   InternalImageType::IndexType  index;
   index[0] = seed_x;
   index[1] = seed_y;
   index[2] = seed_z;
-  connectedThreshold->SetSeed(index);
+
+  double max_image_x_dist_ratio, max_image_y_dist_ratio,
+      max_image_z_dist_ratio;
+  diff->Update();
+  long long i_max = 0;
+  long long j_max = 0;
+  long long k_max = 0;
+  double z_max = 0;
+
+  // Going over all neibghorhood if the seed and trying each of them as the
+  // seed, at the same time ,we make sure that we did not do oversegmentation
+  // and that the length of the spine is not too short.
+  for (int i = -2; i <= 2; i++) {
+    for (int j = -2; j <= 2; j++) {
+      for (int k = -1; k <= 1; k++) {
+        InternalImageType::IndexType seed_copy = index;
+        seed_copy[0] += i;
+        seed_copy[1] += j;
+        seed_copy[2] += k;
+        connectedThreshold->SetSeed(seed_copy);
+        InternalImageType::Pointer image = diff->GetOutput();
+
+        double upper_threshold, lower_threshold;
+        // GrowRegions returning false means that deviation of points was too
+        // big.
+        // The (i == 3 && j == 3 && k == 3) statement makes sure that even
+        // when deviation is too big for all neighbors, we still produce some
+        // output.
+        if (!GrowRegions(image, seed_copy, &upper_threshold, &lower_threshold) &&
+            !(i == 3 && j == 3 && k == 3)){
+          continue;
+        }
+
+        connectedThreshold->SetLower(lower_threshold);
+        connectedThreshold->SetUpper(upper_threshold);
+        connectedThreshold->SetInput(image);
+        connectedThreshold->Update();
+
+        image = connectedThreshold->GetOutput();
+        double max_image_x_dist_ratio, max_image_y_dist_ratio,
+            max_image_z_dist_ratio;
+        ComputeCentroids(image, &max_image_x_dist_ratio, &max_image_y_dist_ratio,
+                         &max_image_z_dist_ratio);
+        // If we don't have oversegmentation, we check if it is the longest
+        // segment we ever saw and if it is the case record the shift values
+        if ((max_image_x_dist_ratio <= 0.33 && max_image_y_dist_ratio <= 0.33 &&
+            max_image_z_dist_ratio >= 0.6) || (i == 3 && j == 3 && k == 3)) {
+          std::cout << "break" << std::endl;
+          if (max_image_z_dist_ratio > z_max) {
+            z_max = max_image_z_dist_ratio;
+            i_max = i;
+            j_max = j;
+            k_max = k;
+          }
+        }
+      }
+    }
+  }
+  // Doing the final segmentation using shift values maximizing the spine
+  // height.
+  InternalImageType::IndexType seed_copy = index;
+  seed_copy[0] += i_max;
+  seed_copy[1] += j_max;
+  seed_copy[2] += k_max;
+  connectedThreshold->SetSeed(seed_copy);
+  image = diff->GetOutput();
 
   double upper_threshold, lower_threshold;
-  GrowRegions(image, index, &upper_threshold, &lower_threshold);
+  GrowRegions(image, seed_copy, &upper_threshold, &lower_threshold);
 
   connectedThreshold->SetLower(lower_threshold);
   connectedThreshold->SetUpper(upper_threshold);
-
   connectedThreshold->SetInput(image);
+  connectedThreshold->Update();
 
-  // Running all filters at once
-  try {
-     connectedThreshold->Update();
-  }
-  catch(itk::ExceptionObject & excep) {
-     std::cerr << "Exception caught !" << std::endl;
-     std::cerr << excep << std::endl;
-  }
   image = connectedThreshold->GetOutput();
-  return ComputeCentroids(image);
+
+  //caster->SetInput(connectedThreshold->GetOutput());
+  //writer->SetInput(caster->GetOutput());
+
+  //writer->Update();
+
+  image = connectedThreshold->GetOutput();
+  return ComputeCentroids(image, &max_image_x_dist_ratio, &max_image_y_dist_ratio,
+    &max_image_z_dist_ratio);
 }
